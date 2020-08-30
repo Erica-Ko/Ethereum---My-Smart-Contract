4bcd240cf4e5958b6410fd1a68c54cd77c2f318d
Erica-Ko <68455198+Erica-Ko@users.noreply.github.com>
2020-08-30 16:14:46 +0800

    Create RestaurantBills.sol

---
+++ RestaurantBills.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.18 <= 0.6.12;
+
+contract restaurantBills {
+    string customerName;
+    int amountOfBill;
+    string dishes;
+    string restaurantName;
+    string Date;
+    string customerAddrs;
+    string invoiceNumber;
+    
+    function restaurantBills (string newCustomerName, int newAmountOfBill, string newDishes, string newRestaurantName, 
+    string newDate, string newCustomerAddrs, string newInvoiceNumber) public {
+        customerName = newCustomerName;
+        amountOfBill = newAmountOfBill;
+        dishes = newDishes;
+        restaurantName = newRestaurantName;
+        Date = newDate;
+        customerAddrs = newCustomerAddrs;
+        invoiceNumber = newInvoiceNumber;
+    }
+    
+    function getCarInvoice () public view returns (string, int, string, string, string, string, string) {
+        return(customerName, amountOfBill, dishes, restaurantName, Date, customerAddrs, invoiceNumber);
+    }
+}
