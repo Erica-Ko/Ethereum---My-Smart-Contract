b29e8443138b03cbb0b7ab1ebf6428580dc003c6
Erica-Ko <68455198+Erica-Ko@users.noreply.github.com>
2020-08-30 16:51:06 +0800

    Update SchoolMarksAnnualReportCard.sol

--- SchoolMarksAnnualReportCard.sol
+++ SchoolMarksAnnualReportCard.sol
@@ -23,4 +23,15 @@ contract schoolMarks {
     function getSchoolMarks () public view returns (string, int, string, string, int, int, int) {
         return(studentName, studentID, classOfStudent, subjects, marks, credits, gpa);
     }
+    
+    function setSchoolMarks (string newStudentName, int newStudentID, string newClassOfStudent, string newSubjects, 
+    int newMarks, int newCredits, int newGpa) public {
+        studentName = newStudentName;
+        studentID = newStudentID;
+        classOfStudent = newClassOfStudent;
+        subjects = newSubjects;
+        marks = newMarks;
+        credits = newCredits;
+        gpa = newGpa;
+    }
 }
